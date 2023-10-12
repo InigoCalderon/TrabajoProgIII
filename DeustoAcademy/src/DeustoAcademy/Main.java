4291711fa514d77d246f9e3262b4cc713a4cb44f
UnaiE <unai.egusquizadel@opendeusto.es>
2023-10-12 14:16:17 +0200

    Creada la estructura inicial del programa con las ventanas iniciales para el inicio de sesión de cada rol y usuario. Incluye las clases de cada rol, con código inicial y no muy detallado para una creación básica de las ventanas iniciales.

--- DeustoAcademy/src/DeustoAcademy/Main.java
+++ DeustoAcademy/src/DeustoAcademy/Main.java
@@ -4,8 +4,12 @@ public class Main {
 	protected String prueba;
 	protected String Unai;
 	protected String Igor;
+	
+	
+	
 	public static void main(String[] args) {
-		
+		Rol nuevo = new Rol();
+		VentanaLogin1 v = new VentanaLogin1(nuevo);
 	}
 	
 }
