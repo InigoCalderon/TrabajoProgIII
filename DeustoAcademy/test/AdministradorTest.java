707248e413bbc1300d4da8268c1ed23bd4303663
UnaiE <unai.egusquizadel@opendeusto.es>
2024-01-03 17:20:17 +0100

    Mejoras en los tests evitando redundancia, aplicando @Before y SetUps.

--- DeustoAcademy/test/AdministradorTest.java
+++ DeustoAcademy/test/AdministradorTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.assertNull;
 
+import org.junit.Before;
+
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -8,81 +10,48 @@ import DeustoAcademy.Administrador;
 
 public class AdministradorTest {
 	
-	protected Administrador administrador;
-	
+	protected Administrador nueva;
+
+	@Before
+	public void setUp() {
+		 nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
+	}
 	
 	@Test
 	public void TestGetNombre() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
+		
 		assertEquals("nombre", nueva.getNombre());
 	}
 	@Test
 	public void TestGetApellido() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
+		
 		assertEquals("apellido",nueva.getApellido() );
 		
 	}
 	@Test
 	public void TestGetDni() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
+		
 		assertEquals("dni",nueva.getDni() );
 	}
 	@Test
 	public void TestGetCorreo() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
+		
 		assertEquals("correo",nueva.getCorreo() );
 	}
 	@Test
 	public void TestGetTelefono() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
+		
 		assertEquals(123,nueva.getTelefono() );
 	}
 	@Test
 	public void TestGetUsuario() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
+		
 		assertEquals("usuario",nueva.getUsuario() );
 	}
 	@Test
 	public void TestGetContraseña() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
+		
 		assertEquals("contraseña",nueva.getContraseña() );
 	}
-	/*/
-	@Test
-	public String TestSetNombre() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
-		assertEquals("modificacion", nueva.setNombre("modificacion"));
-	}
-	@Test
-	public String TestSetApellido() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
-		assertEquals("modificacion", nueva.setApellido("modificacion"));
-	}
-	@Test
-	public String TestSetDni() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
-		assertEquals("modificacion", nueva.setDni("modificacion"));
-	}
-	@Test
-	public String TestSetCorreo() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
-		assertEquals("modificacion", nueva.setCorreo("modificacion"));
-	}
-	@Test
-	public int TestSetTelefono() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
-		assertEquals(1111, nueva.setTelefono(1111));
-	}
-	@Test
-	public String TestSetUsuario() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
-		assertEquals("modificacion", nueva.setUsuario("modificacion"));
-	}
-	@Test
-	public String TestSetContraseña() {
-		Administrador nueva = new Administrador("nombre", "apellido", "dni", "correo", 123, "usuario", "contraseña");
-		assertEquals("modificacion", nueva.setContraseña("modificacion"));
-	}
 	
-	/*/
 }
